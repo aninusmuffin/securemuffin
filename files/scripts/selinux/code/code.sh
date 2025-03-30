d8b1db86a90500f4d260dc7d5440db1b98dc2271
Aninus Partikler <aninuscsalas@getgoogleoff.me>
2025-03-30 17:51:01 +0200

    fix code.sh?

--- files/scripts/selinux/code/code.sh
+++ files/scripts/selinux/code/code.sh
@@ -7,4 +7,4 @@ set -x
 make -f /usr/share/selinux/devel/Makefile code.pp || exit
 /usr/sbin/semodule -i code.pp -v 
 
-/sbin/restorecon -F -R -v /usr/lib/code/
+/sbin/restorecon -F -R -v /usr/share/code/
